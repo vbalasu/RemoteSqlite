7535b828c71785c3159c4c5ef8aeadafde19d462
vbalasu <vbalasu@gmail.com>
2020-03-07 14:16:33 -0800

    Incremented version to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'remote_sqlite',         # How you named your package folder (MyLib)
   packages = ['remote_sqlite'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'RemoteSqlite is a way to easily access a remote Sqlite database and perform queries',   # Give a short description about your library
   author = 'Vijay Balasubramaniam',                   # Type in your name
